23026da9e86fe33570cb4d19283865b52502737c
arthuravila26 <arthur.la@hotmail.com>
2022-04-09 11:29:59 -0300

    Included route to get description

--- app/main.py
+++ app/main.py
@@ -11,3 +11,8 @@ app = FastAPI()
 @app.get('/tabelas', status_code=200)
 def get_todas_descrições():
     return TabelaService().get_all_table()
+
+
+@app.get('/{description}', status_code=200)
+def get_description(description):
+    return JSONResponse(TabelaService().get_description(description))
--- app/service/tabela_service.py
+++ app/service/tabela_service.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from app.models.tabela_nutricional import TabelaNutricional
+from app.utils.exceptions import DescriptionNotFound
 
 
 class TabelaService:
@@ -13,6 +14,13 @@ class TabelaService:
     def get_all_table(self):
         table_json = [self.serialize(table) for table in TabelaNutricional().get_all()]
         return table_json
+
+    def get_description(self, description):
+        try:
+            description = TabelaNutricional.get_by_description(description)
+            return self.serialize(description)
+        except:
+            raise DescriptionNotFound
 
     def serialize(self, data):
         return {
